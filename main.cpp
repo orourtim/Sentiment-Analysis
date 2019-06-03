028f6d600e899cf5f20b37c2a854277c87035aaa
orourtim <orourtim@oregonstate.edu>
2019-06-03 16:07:59 -0700

    Driver to do the classification and output the accuracy.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 /**
  * @author  Timothy O'Rourke
- * @date    2 June 2019
+ * @date    3 June 2019
  * @brief   CS 331 Spring 2019 Sentiment Analysis
  */
 
 #include <iostream>
+#include <iomanip>
 
 #include "Preprocessor.h"
 #include "FeatureVector.h"
@@ -12,82 +13,57 @@
 #include "ClassNode.h"
 #include "NaiveBayesClassifier.h"
 
+void OutputResults(const std::vector<int>& training_results, const std::vector<int>& test_results, PreprocessorData data);
+double GetAccuracy(const std::vector<int>& results, const std::vector<FeatureVector>& vectors);
+
 int main(int argc, char* argv[])
 {
-    // std::set<std::string> vocab = {"tim", "cute", "good"};
-    // FeatureVector f(vocab);
+    Preprocessor preprocessor("trainingSet.txt", "testSet.txt");
+    std::cout << "Preprocessing..." << std::endl;
+    PreprocessorData data = preprocessor.Process();
 
-    // std::cout << "tim exists: " << f.Exists("tim") << std::endl
-    //           << "cute exists: " << f.Exists("cute") << std::endl
-    //           << "good exists: " << f.Exists("good") << std::endl;
+    NaiveBayesClassifier training_classifier(data.training_vector, data.training_vector, data.vocabulary);
+    NaiveBayesClassifier test_classifier(data.training_vector, data.test_vector, data.vocabulary);
+    std::cout << "Classification..." << std::endl;
+    training_classifier.Run();
+    test_classifier.Run();
 
-    // std::cout << "SetExists on \"good\": ";
+    std::vector<int> training_results = training_classifier.GetResults();
+    std::vector<int> test_results = test_classifier.GetResults();
 
-    // f.SetExists("good");
+    std::cout << "Outputting to console and \"results.txt\"..." << std::endl;
+    OutputResults(training_results, test_results, data);
+}
 
-    // std::cout << f.Exists("good") << std::endl;
+void OutputResults(const std::vector<int>& training_results, const std::vector<int>& test_results, PreprocessorData data)
+{
+    double training_accuracy = GetAccuracy(training_results, data.training_vector);
+    double test_accuracy = GetAccuracy(test_results, data.test_vector);
 
-    Preprocessor p("trainingSet.txt", "testSet.txt");
+    std::ofstream result_file("results.txt");
 
-    PreprocessorData d = p.Process();
+    result_file << "Training File: \"trainingSet.txt\", Testing File: \"trainingSet.txt\" --- Accuracy: " << std::setprecision(4) << training_accuracy << "%" << std::endl;
+    result_file << "Training File: \"trainingSet.txt\", Testing File: \"testSet.txt\" --- Accuracy: " << std::setprecision(4) << test_accuracy << "%" << std::endl;
 
-    // FeatureNode node("nice");
-    // node.FillProbabilityTable(d.training_vector);
-    // double prob1 = node.GetProbability(false, false);
-    // double prob2 = node.GetProbability(false, true);
-    // double prob3 = node.GetProbability(true, false);
-    // double prob4 = node.GetProbability(true, true);
+    std::cout << "Training File: \"trainingSet.txt\", Testing File: \"trainingSet.txt\" --- Accuracy: " << std::setprecision(4) << training_accuracy << "%" << std::endl;
+    std::cout << "Training File: \"trainingSet.txt\", Testing File: \"testSet.txt\" --- Accuracy: " << std::setprecision(4) << test_accuracy << "%" << std::endl;
 
-    // std::cout.precision(2);
-    // std::cout << "P(\"nice\" = false | classlabel = false) = " << std::fixed << prob1 << std::endl;
-    // std::cout << "P(\"nice\" = false | classlabel = true) = " << std::fixed << prob2 << std::endl;
-    // std::cout << "P(\"nice\" = true | classlabel = false) = " << std::fixed << prob3 << std::endl;
-    // std::cout << "P(\"nice\" = true | classlabel = true) = " << std::fixed << prob4 << std::endl;
+    result_file.close();
+}
 
-    // ClassNode cNode;
-    // cNode.FillProbabilityTable(d.training_vector);
-    // double prob5 = cNode.GetProbability(false);
-    // double prob6 = cNode.GetProbability(true);
-
-    std::cout.precision(2);
-    // std::cout << "P(classlabel = false) = " << std::fixed << prob5 << std::endl;
-    // std::cout << "P(classlabel = true) = " << std::fixed << prob6 << std::endl;
-
-    NaiveBayesClassifier classifier(d.training_vector, d.test_vector, d.vocabulary);
-    classifier.Run();
-
-    std::vector<int> results = classifier.GetResults();
-
+double GetAccuracy(const std::vector<int>& results, const std::vector<FeatureVector>& vectors)
+{
     int correct = 0;
     int total = 0;
-    auto test = d.test_vector.begin();
+    auto feature_vector = vectors.begin();
     auto result = results.begin();
-    for (test, result; result != results.end(); ++test, ++result)
+    for (feature_vector, result; result != results.end(); ++feature_vector, ++result)
     {
-        if (*result == test->GetClassLabel())
+        if (*result == feature_vector->GetClassLabel())
         {
             correct++;
         }
         total++;
     }
-    std::cout << "trainingSet.txt, testSet.txt --- Accuracy: " << std::fixed << ((correct * 1.0) / total) * 100 << "%"<< std::endl;
-
-    NaiveBayesClassifier classifier2(d.training_vector, d.training_vector, d.vocabulary);
-    classifier2.Run();
-
-    std::vector<int> results2 = classifier2.GetResults();
-
-    int correct2 = 0;
-    int total2 = 0;
-    auto test2 = d.training_vector.begin();
-    auto result2 = results2.begin();
-    for (test2, result2; result2 != results2.end(); ++test2, ++result2)
-    {
-        if (*result2 == test2->GetClassLabel())
-        {
-            correct2++;
-        }
-        total2++;
-    }
-    std::cout << "trainingSet.txt, trainingSet.txt --- Accuracy: " << std::fixed << ((correct2 * 1.0) / total2) * 100 << "%"<< std::endl;
+    return ((correct * 1.0) / total) * 100;
 }
